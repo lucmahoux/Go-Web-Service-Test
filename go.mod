dacf5b455b0277e3f8662069f1af0f51869e0534
Luc Mahoux <luc.mahoux@epita.fr>
2023-01-29 23:18:29 +0100

    feat (httpserv): modify dependencies

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/lucmahoux/go_http_test
+
+go 1.19
--- main.go
+++ main.go
@@ -2,17 +2,19 @@ package main
 
 import ( 
     "net/http"
-    "working/handlers"
+    "github.com/lucmahoux/go_http_test/handlers"
     "log"
     "os"
 )
 
 func main() {
     l := log.New(os.Stdout, "product-api", log.LstdFlags)
-    hello := handlers.NewHello()
+    hello := handlers.NewHello(l)
+    goodbye := handlers.NewGoodbye(l)
 
     serveMux := http.NewServeMux()
-    serveMux.Handle("/", hh)
+    serveMux.Handle("/", hello)
+    serveMux.Handle("/goodbye", goodbye)
 
-    http.ListenAndServe(":9090", nil)
+    http.ListenAndServe(":9090", serveMux)
 }
