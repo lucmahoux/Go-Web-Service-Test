0adc5f8723a86baaeac98c0c6bbbfc700f6b5e2c
Luc Mahoux <luc.mahoux@epita.fr>
2023-01-30 12:56:40 +0100

    feat (product): add product POST handler

--- handlers/products.go
+++ handlers/products.go
@@ -19,8 +19,11 @@ func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
         p.getProducts(rw, r)
         return
     }
-
-    // handle UPDATE
+    
+    if r.Method == http.MethodPost {
+        p.addProduct(rw, r)
+        return
+    }
 
     // catch all
     rw.WriteHeader(http.StatusMethodNotAllowed)
@@ -32,4 +35,17 @@ func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request){
     if err != nil {
         http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
     }
+}
+
+func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
+    p.l.Println("Handle POST Product")
+
+    prod := &data.Product{}
+
+    err := prod.FromJSON(r.Body)
+    if err != nil {
+        http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
+    }
+
+    data.AddProduct(prod)
 }
