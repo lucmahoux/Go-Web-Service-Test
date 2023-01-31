6ae1792f18e5c722a91a1318655917ba674faaa7
Luc Mahoux <luc.mahoux@epita.fr>
2023-01-31 18:03:49 +0100

    feat (product): add validation to the middleware

--- data/products_test.go
+++ data/products_test.go
@@ -6,6 +6,7 @@ func TestChecksValidation(t *testing.T) {
     p := &Product{
         Name: "coffee",
         Price: 1.00,
+        SKU: "abs-abc-def",
     }
 
     err := p.Validate()
--- handlers/products.go
+++ handlers/products.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"strconv"
@@ -66,13 +67,29 @@ func (p Products) MiddlewareProductValidation(next http.Handler) http.Handler {
 
         err := prod.FromJSON(r.Body)
         if err != nil {
-            http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
+            p.l.Println("[ERROR] deserializing product", err)
+            http.Error(rw, "Error reading product", http.StatusBadRequest)
+            return
+        }
+        
+        //validate the product
+        err = prod.Validate()
+        if err != nil {
+            p.l.Println("[ERROR] validate product", err)
+            http.Error(
+                rw, 
+                fmt.Sprintf("Error validating product: %s", err),
+                http.StatusBadRequest,
+            )
             return
         }
 
+        // add the product to the context
         ctx := context.WithValue(r.Context(), KeyProduct{}, prod)
         req := r.WithContext(ctx)
 
+        // call the next handler, which can be another middleware in the chain,
+        // or the final handler
         next.ServeHTTP(rw, req)
     })
 }
